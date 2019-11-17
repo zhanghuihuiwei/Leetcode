1d0cbf79c73bf0a8337d367a7f0393e7ffd0ae11
Kalerandoni Tons <41605965+zhanghuihuiwei@users.noreply.github.com>
2019-11-17 17:10:23 +0800

    Update 19删除链表的第n个节点.cpp

--- 19删除链表的第n个节点.cpp
+++ 19删除链表的第n个节点.cpp
@@ -17,3 +17,32 @@ public:
         return head; 
     }
 };
+#上面这个方法实现起来，耗时太大，不好；
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+     ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+
+        ListNode* p = dummyHead;
+        ListNode* q = dummyHead;
+        for( int i = 0 ; i < n + 1 ; i ++ ){
+            q = q->next;
+        }
+
+        while(q){
+            p = p->next;
+            q = q->next;
+        }
+
+        ListNode* delNode = p->next;
+        p->next = delNode->next;
+        delete delNode;
+
+        ListNode* retNode = dummyHead->next;
+        delete dummyHead;
+
+        return retNode;
+        
+    }
+};
